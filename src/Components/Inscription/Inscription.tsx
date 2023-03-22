5dccb242b0923037c067c1b1e5f5a0dee2b8541b
ElemenT <xetoxin@gmail.com>
2023-03-22 14:09:27 +0100

    starting with firebase auth

--- src/Components/Inscription/Inscription.tsx
+++ src/Components/Inscription/Inscription.tsx
@@ -1,28 +1,68 @@
 import React from 'react';
-import {firebaseConfig} from '../../config/config';
+import config from '../../config/config';
 import {initializeApp} from 'firebase/app';
-import {getAuth} from 'firebase/auth';
+import {getAuth, createUserWithEmailAndPassword} from 'firebase/auth';
 import {useState} from 'react';
+import {getFirestore} from 'firebase/firestore';
 
-initializeApp(firebaseConfig);
+initializeApp(config.firebaseConfig);
+
 function Inscription() {
 	const auth = getAuth();
+	const db = getFirestore();
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-	const [loading, setLoading] = useState(false);
+	createUserWithEmailAndPassword(auth, email, password)
+		.then((userCredential) => {
+			const user = userCredential.user;
+			console.log('user créer', user.email);
+		})
+		.catch((error) => {
+			const errorCode = error.code;
+			const errorMessage = error.message;
+			console.error(`Erreur lors de la création de l'utilisateur : ${errorCode} - ${errorMessage}`);
+		});
+
+	// Récupérer les inputs de l'utilisateur et les stocker dans la variable setEmail
+	const handleEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setEmail(event.target.value);
+		console.log('email', email);
+	};
+	// Récupéer les inputs de l'utilisateur et les stocker dans la variable setPassword
+	const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setPassword(event.target.value);
+	};
+
 	return (
 		<div className='inscription-container'>
 			<form action='submit' className='inscription-form'>
 				<label htmlFor='Email'>
 					Email
-					<input type='email' name='email' placeholder='VotreEmail@exemple.com' className='inscription-input' />
+					<input
+						id='email'
+						type='email'
+						name='email'
+						value={email}
+						onChange={handleEmail}
+						placeholder='VotreEmail@exemple.com'
+						className='inscription-input'
+					/>
 				</label>
 
 				<label htmlFor='password'>
 					Mot de passe
-					<input type='password' name='password' placeholder='Votre mot de passe' className='inscription-input' />
+					<input
+						type='password'
+						name='password'
+						value={password}
+						onChange={handlePassword}
+						placeholder='Votre mot de passe'
+						className='inscription-input'
+					/>
 				</label>
+				<button type='submit'>Envoyer</button>
 			</form>
-			<button type='submit'>Envoyer</button>
 		</div>
 	);
 }
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,13 @@
-export const firebaseConfig = {
-	apiKey: 'AIzaSyD_XGloIbjUMrd8YWvBSpYTEXSjGs5KAR4',
-	authDomain: 'taiwan-50060.firebaseapp.com',
-	projectId: 'taiwan-50060',
-	storageBucket: 'taiwan-50060.appspot.com',
-	messagingSenderId: '147739918059',
-	appId: '1:147739918059:web:5d5ea3d7afee3e343d48ae',
-	measurementId: 'G-39F7XJPY1Y',
+const config = {
+	firebaseConfig: {
+		apiKey: 'AIzaSyD_XGloIbjUMrd8YWvBSpYTEXSjGs5KAR4',
+		authDomain: 'taiwan-50060.firebaseapp.com',
+		projectId: 'taiwan-50060',
+		storageBucket: 'taiwan-50060.appspot.com',
+		messagingSenderId: '147739918059',
+		appId: '1:147739918059:web:5d5ea3d7afee3e343d48ae',
+		measurementId: 'G-39F7XJPY1Y',
+	},
 };
+
+export default config;
